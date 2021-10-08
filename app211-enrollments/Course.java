54fb465f1480cd2e417b0b000293b6f01c74a226
Brandon-Farell <91319316+Brandon-Farell@users.noreply.github.com>
2021-10-08 12:34:39 +0100

    Update Course.java

--- app211-enrollments/Course.java
+++ app211-enrollments/Course.java
@@ -4,8 +4,8 @@ import java.util.*;
  * This class contains information about an undergraduate course
  * at BNU together with a list of student who are enrolled
  *
- * @author Derek Peacock
- * @version 1.0 14/07/2021
+ * @author Brandon Farrell
+ * @version 2.0 08/10/2021
  */
 public class Course
 {
